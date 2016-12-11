df01c2f864895796c1cfd5121c2df001efe7e757
reazul hoque riyad <reazul hoque riyad>
2016-12-11 19:25:12 +0600

    downgrade version

---
+++ database/migrations/2016_12_11_130128_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category_name',100);
+            $table->enum('status',['Active','Deleted'])->default('Active');
+            $table->integer('creatd_by');
+            $table->integer('updated_by')->nullable()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Schema::rename('lab_test_category', 'lab_test_categories');
+        Schema::drop('categories');
+    }
+}
