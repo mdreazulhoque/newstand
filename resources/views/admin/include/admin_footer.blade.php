6129fd9f1415295a9148284dc76f6c2e2b5e923d
Taiful <taiful2514@gmail.com>
2016-12-11 22:43:52 +0600

    user template

---
+++ resources/views/admin/include/admin_footer.blade.php
@@ -0,0 +1,42 @@
+
+
+<!-- Bootstrap Core Js -->
+<script src="{{asset('developer/admin/plugins/bootstrap/js/bootstrap.js')}}"></script>
+
+
+
+
+
+<!-- Waves Effect Plugin Js -->
+<script src="{{asset('developer/admin/plugins/node-waves/waves.js')}}"></script>
+
+
+
+
+
+
+<!-- Autosize Plugin Js -->
+<script src="{{asset('developer/admin/plugins/autosize/autosize.js')}}"></script>
+
+
+
+
+
+<!-- Jquery DataTable Plugin Js -->
+<script src="{{asset('developer/admin/plugins/jquery-datatable/jquery.dataTables.js')}}"></script>
+<script src="{{asset('developer/admin//plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js')}}"></script>
+<script src="{{asset('developer/admin/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('developer/admin/plugins/jquery-datatable/extensions/export/buttons.flash.min.js')}}"></script>
+<script src="{{asset('developer/admin/plugins/jquery-datatable/extensions/export/jszip.min.js')}}"></script>
+<script src="{{asset('developer/admin/plugins/jquery-datatable/extensions/export/pdfmake.min.js')}}"></script>
+<script src="{{asset('developer/admin/plugins/jquery-datatable/extensions/export/vfs_fonts.js')}}"></script>
+<script src="{{asset('developer/admin/plugins/jquery-datatable/extensions/export/buttons.html5.min.js')}}"></script>
+<script src="{{asset('developer/admin/plugins/jquery-datatable/extensions/export/buttons.print.min.js')}}"></script>
+
+
+<!-- Custom Js -->
+<script type="text/javascript" src="{{asset('developer/admin/js/admin.js')}}"></script>
+<script type="text/javascript" src="{{asset('developer/admin/js/pages/index.js')}}"></script>
+
+<!-- Demo Js -->
+<script type="text/javascript" src="{{asset('developer/admin/js/demo.js')}}"></script>
