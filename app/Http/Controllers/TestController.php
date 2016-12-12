2e22b3d751c7d40d2717a168e8504c92912ab8a6
Rafsan <rafsanhasin@gmail.com>
2016-12-13 02:36:42 +0600

    login/reg view added

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,10 +9,15 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class TestController extends BaseNewsController
 {
-    public function index(){
+    public function index(Request $request){
 
+        return json_encode(Auth::guard("web"));
+        return view('user.email_verification');
         $stop_date = '2009-09-30 20:24:00';
         //echo 'date before day adding: ' . $stop_date;
         $stop_date = date('Y-m-d H:i:s', strtotime($stop_date . ' +1 day'));
---
+++ resources/views/user/email_verification.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User-Login</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('developer/user/css/reg.css')}}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{asset('developer/user/css/reg_demo.css')}}" media="all" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <!-- freshdesignweb top bar -->
+    <div class="freshdesignweb-top">
+        <a href="{{\Illuminate\Support\Facades\URL::to('home')}}">Home</a>
+                <span class="right">
+                    <a href="{{\Illuminate\Support\Facades\URL::to('user/register/view')}}">
+                        <strong>Register</strong>
+                    </a>
+                </span>
+        <div class="clr"></div>
+    </div><!--/ freshdesignweb top bar -->
+    <header>
+        <h1>Email Verification</h1>
+        <div>Your Email is successfully verified. Set your Password</div>
+
+    </header>
+
+    <div  class="form">
+        <div id="contactform">
+
+            <p class="contact"><label for="name">Password</label></p>
+            <input id="password" name="password" placeholder="Password" required="" tabindex="1" type="password">
+
+            <p class="contact"><label for="name">Confirm Password</label></p>
+            <input id="confirm_password" name="password" placeholder="Confirm Password" required="" tabindex="1" type="password">
+
+            <br>
+            <input class="buttom" name="submit" id="submit" tabindex="5" value="Sign me up!" type="button" onclick="login()">
+        </div>
+        <div id="notification"></div>
+    </div>
+</div>
+<input type="hidden" id="baseUrl" value="{{url('/')}}/"/>
+<input type="hidden" id="token" value="{{Session::get('token')}}"/>
+</body>
+
+<script>
+
+    function login() {
+
+        if($('#token').val() ==""){
+            $('#notification').html("Go back to your verification link and try again!");
+            window.setTimeout(function() {
+                window.location.href = $('#baseUrl').val() + 'home';
+            }, 3000);
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: $('#baseUrl').val() + 'user/password/set/'+$('#token').val(),
+            data: {
+                password: $('#password').val(),
+                confirm_password: $('#confirm_password').val(),
+            },
+            success: function (data) {
+
+
+                if (data.responseStat.status == true) {
+
+                    $('#notification').html(data.responseStat.msg);
+                    window.setTimeout(function() {
+                        window.location.href = $('#baseUrl').val() + 'user/login/view';
+                    }, 3000);
+
+                } else {
+                    $('#notification').html(data.responseStat.msg);
+                }
+
+
+            },
+            error: function () {
+                alert('Error occured');
+            }
+        });
+
+    }
+
+
+</script>
+</html>
